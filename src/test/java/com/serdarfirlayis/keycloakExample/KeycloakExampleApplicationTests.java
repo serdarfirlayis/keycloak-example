cb40a011169542734b584a01748d029aed022699
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2022-09-04 18:47:34 +0300

    initial commit

---
+++ src/test/java/com/serdarfirlayis/keycloakExample/KeycloakExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serdarfirlayis.keycloakExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
